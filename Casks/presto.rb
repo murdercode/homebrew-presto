990b800c0425c8fe0a4ab67236e4e455c45e266b
Stefano Novelli <murdercode@gmail.com>
2025-06-19 18:04:56 +0200

    Update Presto to version 0.2.11

--- Casks/presto.rb
+++ Casks/presto.rb
@@ -1,13 +1,13 @@
 cask "presto" do
-  version "0.2.9"
+  version "0.2.11"
   sha256 :no_check  # Will be updated with actual SHA256 when you have a release
 
   on_arm do
-    url "https://github.com/murdercode/presto/releases/download/v#{version}/presto_#{version}_aarch64.dmg"
+    url "https://github.com/murdercode/presto/releases/download/v#{version}/presto_#{version}_aarch64.app.tar.gz"
   end
 
   on_intel do
-    url "https://github.com/murdercode/presto/releases/download/v#{version}/presto_#{version}_x64.dmg"
+    url "https://github.com/murdercode/presto/releases/download/v#{version}/presto_#{version}_x64.app.tar.gz"
   end
 
   name "Presto"
